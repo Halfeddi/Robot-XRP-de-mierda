a70e766ddb78d559d7bb490743307931de3cea45
Halfeddi <dereckhumes@gmail.com>
2024-05-14 20:56:53 -0500

    hola

---
+++ XRP controller/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,135 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.Drivetrain;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
+  private Joystick ps5;
+  Drivetrain drivetrain;
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+    drivetrain = Drivetrain.getInstance();
+    // PS5Controller ps5Controller = new PS5Controller(0);
+    ps5 = new Joystick(0);
+
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+  public static double roundToZero(double value, double threshold){
+    return Math.abs(value) < threshold ? 0.0 : value;
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    double forwardSpeed = -ps5.getRawAxis(1);
+    double rotationSpeed = ps5.getRawAxis(4);
+
+    rotationSpeed = roundToZero(rotationSpeed, 0.1);
+    forwardSpeed = roundToZero(forwardSpeed, 0.05);
+
+    // Calculate left and right motor speeds
+    double leftMotorSpeed = forwardSpeed + rotationSpeed;
+    double rightMotorSpeed = forwardSpeed - rotationSpeed;
+
+    // Ensure motor speeds are within valid range [-1, 1]
+    leftMotorSpeed = Math.max(-1, Math.min(1, leftMotorSpeed));
+    rightMotorSpeed = Math.max(-1, Math.min(1, rightMotorSpeed));
+
+    // Print debug information
+    System.out.print("Rotation: " + rotationSpeed);
+    System.out.println(" | Left Motor Speed: " + leftMotorSpeed + " | Right Motor Speed: " + rightMotorSpeed);
+
+    // Drive the robot using tank drive
+    drivetrain.tankDrive(leftMotorSpeed, rightMotorSpeed);
+  }
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
